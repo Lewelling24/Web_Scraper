eadac2f34c893f6a59e6fdca0ff641686ce5455b
Kane Lewelling <kanelewelling@gmail.com>
2024-05-07 18:56:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from urllib.request import urlopen
 # just getting started
 url = urlopen("https://google.com")
 
-print(url)
+html_bytes = url.read()
+
+html = html_bytes.decode("utf-8")
+
+print(html)
