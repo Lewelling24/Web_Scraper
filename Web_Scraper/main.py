1d20872cc6f781613f9af7fcc355d0b1dace817c
Kane Lewelling <kanelewelling@gmail.com>
2024-05-16 00:11:03 -0400

    Add files via upload

--- Web_Scraper/main.py
+++ Web_Scraper/main.py
@@ -10,11 +10,19 @@ url = "https://www.imdb.com/chart/top"
 
 response = requests.get(url)
 
+print (response)
+
 parse = BeautifulSoup(response.content, 'html.parser')
+
+print(parse)
 
 # extract info
 movies = []
 
 for row in parse.select('tbody.lister-list tr'):
     title = row.find('td', class_='titleColumn').find('a').get_text()
-print(parse)
+    year = row.find('td', class_='titleColumn').find('span', class_='secondaryInfo').get_text()[1:-1]
+    rating = row.find('td', class_='ratingColumn imdbRating').find('strong').get_text()
+    movies.append([title, year, rating])
+
+print(movies)
