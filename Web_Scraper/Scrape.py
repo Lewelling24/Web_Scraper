d4af76950407fdf7acb714c9a7e203ee1294acf2
Kane Lewelling <kanelewelling@gmail.com>
2024-05-23 19:42:04 -0400

    Add files via upload

---
+++ Web_Scraper/Scrape.py
@@ -0,0 +1,46 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+import pandas as pd
+
+import time
+
+url = "https://www.imdb.com/chart/top"
+
+#transfer into function
+
+def scrape(url):
+    response = requests.get(url)
+    parse = BeautifulSoup(response.content, 'html.parser')
+    # extract info
+    movies = []
+
+    for row in parse.select('tbody.lister-list tr'):
+        title = row.find('td', class_='titleColumn').find('a').get_text()
+        year = row.find('td', class_='titleColumn').find('span', class_='secondaryInfo').get_text()[1:-1]
+        rating = row.find('td', class_='ratingColumn imdbRating').find('strong').get_text()
+        movies.append([title, year, rating])
+
+    
+    return movies    
+    # Add a delay between requests to avoid overwhelming the website with requests
+    time.sleep(1)
+
+
+def store(movies):
+# Store the information in a pandas dataframe
+    df = pd.DataFrame(movies, columns=['Title', 'Year', 'Rating'])
+    df.to_csv('top-rated-movies.csv', index=False)
+
+    
+time.sleep(1)
+
+
+
+
+
+
+
+
+
