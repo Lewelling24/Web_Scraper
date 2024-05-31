b9b2f57b19b390ba793be6c9f8f41ea20348a541
Kane Lewelling <kanelewelling@gmail.com>
2024-05-30 20:49:53 -0400

    Add files via upload

--- Web_Scraper/Scrape.py
+++ Web_Scraper/Scrape.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 def scrape(url):
     response = requests.get(url)
     parse = BeautifulSoup(response.content, 'html.parser')
-    # extract info
+    
     return parse
 
     
